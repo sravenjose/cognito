0949b283b185f55430efc8b976f93cf28b2e49db
sraven <sraven.jose@gmail.com>
2022-04-06 12:48:39 +0530

    commit all changes

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  cognito: {
-    userPoolId: 'ap-south-1_F9nlKUaJV',
-    userPoolWebClientId: '7ssripghg9lmbljvmcmo2qnvkf',
-    authenticationFlowType:'CUSTOM_AUTH'
-  },
-};
